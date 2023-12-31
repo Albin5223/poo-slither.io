41779637aee2fc2cabf9b9905666f34da6775a2c
YAZICI Servan <sservxf@gmail.com>
2023-12-31 02:19:25 +0100

    PixelFont & button class

---
+++ app/src/main/java/GUI/ButtonPixelFont.java
@@ -0,0 +1,32 @@
+package GUI;
+
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Paint;
+
+public class ButtonPixelFont extends Button {
+
+    private TextPixelFont textPixelFont;
+
+    public ButtonPixelFont(String text, int size) {
+        this.textPixelFont = new TextPixelFont(text, size);
+        this.setGraphic(textPixelFont);
+
+        // Set the style of the button
+        this.setStyle("-fx-padding: 0; -fx-background-color: transparent;");
+
+        // Change the cursor when it hovers over the button
+        this.setOnMouseEntered(e -> this.setCursor(Cursor.HAND));  // Change to hand cursor
+        this.setOnMouseExited(e -> this.setCursor(Cursor.DEFAULT));  // Change back to default cursor
+
+        // Set the size of the button to be the same as the size of the text
+        this.setPrefWidth(textPixelFont.getLayoutBounds().getWidth());
+        this.setPrefHeight(textPixelFont.getLayoutBounds().getHeight());
+    }
+
+    public void setFill(Paint value) {
+        textPixelFont.setFill(value);
+    }
+
+    
+}
---
+++ app/src/main/java/GUI/TextPixelFont.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class TextPixelFont extends Text {
+
+    public static final double STROKE_RATIO = 0.04;
+    public static final double SHADOW_OFFSET_RATIO = 0.025;
+
+    public TextPixelFont(String text, int size) {
+        super(text);
+        this.setStroke(Color.BLACK);
+        this.setStrokeWidth(size*STROKE_RATIO);
+        this.setFill(Color.WHITE);
+        Font customFont = Font.loadFont(getClass().getResource("/PixelFontYS.ttf").toExternalForm(), size);
+        this.setFont(customFont);
+
+        // Add drop shadow effect
+        DropShadow ds = new DropShadow();
+        ds.setOffsetY(size * SHADOW_OFFSET_RATIO);
+        ds.setOffsetX(size * SHADOW_OFFSET_RATIO);
+        ds.setColor(Color.color(0, 0, 0, 1));  // Set the opacity to 100%
+        ds.setRadius(1.0);  // Reduce the radius to make the shadow less blurry
+        this.setEffect(ds);
+    }
+    
+}
